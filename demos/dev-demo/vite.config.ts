62ddd664d8d1e2277698f5abb21c7798ded01b6c
Zorro <zorro11639@gmail.com>
2024-09-23 17:20:09 +0800

    chore: add node-polyfill for vite-app demo

--- demos/dev-demo/vite.config.ts
+++ demos/dev-demo/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,12 @@ export default defineConfig({
         legacy({
             targets: ['IE 11'],
         }),
+        nodePolyfills({
+            include: ['crypto', 'buffer', 'stream'],
+            globals: {
+                Buffer: true,
+            }
+        })
     ],
     resolve: {
         alias: {
